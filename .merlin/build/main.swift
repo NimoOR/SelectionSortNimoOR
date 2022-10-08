771522cb8ee38bf505687019917ab07fc0f69040
NimoOR <nimi.roza@gmail.com>
2022-10-08 12:54:46 -0500

    selection sort

---
+++ .merlin/build/main.swift
@@ -0,0 +1,32 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let unsortedIntegers = [603, 447, -663, 611, 589, 821, 548, 495]
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+
+func selectSort(inputArr: [Int]){
+    var arr = inputArr
+    var swap = 0
+    var count = 0
+    for i in 0..<arr.count {
+        print("Pass: \(i), Swaps: \(swap)/\(count), Array: \(arr)")
+        swap = 0
+        var min = i
+        for j in (i+1)..<arr.count {
+            if arr[min] > arr[j]{
+                min = j
+            }
+        }
+        (arr[i], arr[min]) = (arr[min], arr[i])
+        count += 1
+        swap += 1
+    }
+}
+
+
+selectSort(inputArr: unsortedIntegers)
